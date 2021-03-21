5fad10f495f62a39bbcaaa5f61cac8a07e162cdb
Godday Okoduwa <goddayokos@gmail.com>
2021-03-21 18:58:48 +0100

    Implemented login and authentication page through MS web auth

--- app/src/main/java/com/decagon/facilitymanagementapp_group_two/ui/OnboardingFragment.kt
+++ app/src/main/java/com/decagon/facilitymanagementapp_group_two/ui/OnboardingFragment.kt
@@ -5,12 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.decagon.facilitymanagementapp_group_two.R
 import com.decagon.facilitymanagementapp_group_two.databinding.OnboardingFragmentBinding
+import com.decagon.facilitymanagementapp_group_two.ms_auth.MsWebAuthentication
+import com.microsoft.identity.client.ISingleAccountPublicClientApplication
 
 class OnboardingFragment : Fragment() {
     private var _binding: OnboardingFragmentBinding? = null
     private val binding
         get() = _binding!!
+
+    companion object {
+        lateinit var mSingleAccountApp: ISingleAccountPublicClientApplication
+    }
+
+    private val scopes = arrayOf("user.read")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,10 +32,16 @@ class OnboardingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        MsWebAuthentication.initialiseSingleAccount(requireContext(), R.raw.auth_config_single_account)
         /**
          * Click listener for the get started button
          */
         binding.fragmentOnboardGetStartedBtn.setOnClickListener {
+            mSingleAccountApp.signIn(
+                requireActivity(), null, scopes,
+                MsWebAuthentication.getAuthenticationCallback(this@OnboardingFragment)
+            )
         }
     }
 }
