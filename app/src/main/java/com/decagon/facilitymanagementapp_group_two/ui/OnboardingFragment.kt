e27286188873fa4399ea9ceb1fd7bcf07e66d55a
Tolulope Longe <tolulopelonge945@gmail.com>
2021-03-22 00:15:40 +0100

    changed toolbar color and height

--- app/src/main/java/com/decagon/facilitymanagementapp_group_two/ui/OnboardingFragment.kt
+++ app/src/main/java/com/decagon/facilitymanagementapp_group_two/ui/OnboardingFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.decagon.facilitymanagementapp_group_two.R
 import com.decagon.facilitymanagementapp_group_two.databinding.OnboardingFragmentBinding
 
 class OnboardingFragment : Fragment() {
@@ -27,6 +29,7 @@ class OnboardingFragment : Fragment() {
          * Click listener for the get started button
          */
         binding.fragmentOnboardGetStartedBtn.setOnClickListener {
+            findNavController().navigate(R.id.singleComplaintFragment)
         }
     }
 }
