073d70c0b72584cdbbad5ea66d13c2c18266d015
Tolulope Longe <tolulopelonge945@gmail.com>
2021-03-21 12:21:17 +0100

    added fragment-test artifact to dependencies

---
+++ app/src/androidTest/java/com/decagon/facilitymanagementapp_group_two/ui/OnboardingFragmentTest.kt
@@ -0,0 +1,12 @@
+package com.decagon.facilitymanagementapp_group_two.ui
+
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class OnboardingFragmentTest {
+
+}
+
