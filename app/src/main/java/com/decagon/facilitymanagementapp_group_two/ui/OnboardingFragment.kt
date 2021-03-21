1593cfcb7b341b7b6579ace5f09ec036cab842c2
Tolulope Longe <tolulopelonge945@gmail.com>
2021-03-22 00:24:17 +0100

    removed navigation from onboard

--- app/src/main/java/com/decagon/facilitymanagementapp_group_two/ui/OnboardingFragment.kt
+++ app/src/main/java/com/decagon/facilitymanagementapp_group_two/ui/OnboardingFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.decagon.facilitymanagementapp_group_two.R
 import com.decagon.facilitymanagementapp_group_two.databinding.OnboardingFragmentBinding
 
 class OnboardingFragment : Fragment() {
@@ -29,7 +27,6 @@ class OnboardingFragment : Fragment() {
          * Click listener for the get started button
          */
         binding.fragmentOnboardGetStartedBtn.setOnClickListener {
-            findNavController().navigate(R.id.singleComplaintFragment)
         }
     }
 }
