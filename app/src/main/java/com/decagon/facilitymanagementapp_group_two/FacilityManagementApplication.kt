15e98516cdce60b877e6610afcf6debf28b17456
Tolulope Longe <tolulopelonge945@gmail.com>
2021-03-25 15:57:38 +0100

    added hilt application class

---
+++ app/src/main/java/com/decagon/facilitymanagementapp_group_two/FacilityManagementApplication.kt
@@ -0,0 +1,8 @@
+package com.decagon.facilitymanagementapp_group_two
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FacilityManagementApplication : Application() {
+}
