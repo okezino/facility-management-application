99a690cf1d7ae894f5bf7a6d088af30afef4c4cd
okezino <okezi002@gmail.com>
2021-03-29 00:23:43 +0100

    setup test

--- app/src/androidTest/java/com/decagon/facilitymanagementapp_group_two/ui/SubmitFragmentTest.kt
+++ app/src/androidTest/java/com/decagon/facilitymanagementapp_group_two/ui/SubmitFragmentTest.kt
@@ -1,12 +1,20 @@
 package com.decagon.facilitymanagementapp_group_two.ui
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.decagon.facilitymanagementapp_group_two.R
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.core.Is
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -32,6 +40,16 @@ class SubmitFragmentTest{
         onView(withId(R.id.request_subject_layout)).check(matches(isDisplayed()))
         onView(withId(R.id.select_feed_category)).check(matches(isDisplayed()))
         onView(withId(R.id.feed_category_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.back_btn)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_submit)).check(matches(isDisplayed()))
+
+
+        onView(withId(R.id.btn_submit)).perform(click())
+        onView(withId(R.id.request_subject)).perform(typeText("kitchen"), closeSoftKeyboard())
+        onView(withId(R.id.request_description)).perform(typeText("kitchen food is too small"), closeSoftKeyboard())
+        onView(withId(R.id.btn_submit)).perform(click())
+        onView(withId(R.id.feed_category_layout)).perform(click())
+
 
 
 
