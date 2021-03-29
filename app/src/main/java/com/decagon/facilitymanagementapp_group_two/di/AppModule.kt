122507dcc2e78de2d96483f7844ca1f503d42291
Tolulope Longe <tolulopelonge945@gmail.com>
2021-03-29 04:01:47 +0100

    created the appModule and added application class to manifest

---
+++ app/src/main/java/com/decagon/facilitymanagementapp_group_two/di/AppModule.kt
@@ -0,0 +1,16 @@
+package com.decagon.facilitymanagementapp_group_two.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
